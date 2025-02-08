9711c9a51d5ff259cbbf926d3098e0bdb302c8a2
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-08 13:34:59 +0530

    console.log of both createElement and JSX

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom/client'
 const heading = React.createElement('h1', {}, "This is First Code in React");
         
 const div1 = React.createElement('div', { id: 'container' },[ heading , "Hello Everyone"]);
+console.log(div1);
+
 
 //  Using JSX
 //  JSX (Transpiled before reaches the JS) => (Transpiled is done by the parcel library known as Babel)
@@ -12,6 +14,10 @@ const jsxusing = <div id="container">
                     <h1> This is First Code Using JSX in React</h1>
                     <p>Hello Everyone</p>
                 </div>;
+
+console.log(jsxusing);
+// Both the have the same output as Object because of Babel  
+
 
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root1.render(jsxusing);
