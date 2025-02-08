734caaf83adde17743df3750f3e255302491e884
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-08 12:03:32 +0530

    delete

--- dist/index.6bd02f5a.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement('h1', {}, "This is First Code in React");
-const div1 = React.createElement('div', {
-    id: 'container'
-}, heading, "Hello Everyone");
-const root1 = ReactDOM.createRoot(document.body);
-root1.render(div1);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
