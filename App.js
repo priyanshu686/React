7f4079937870a0d7d940ea0cd1f1b652b3e68b1d
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-08 21:51:55 +0530

    Learn React Component and Render it

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
-// Using createElement  
-const heading = React.createElement('h1', {}, "This is First Code in React");
-        
-const div1 = React.createElement('div', { id: 'container' },[ heading , "Hello Everyone"]);
-console.log(div1);
-
-
 //  Using JSX
 //  JSX (Transpiled before reaches the JS) => (Transpiled is done by the parcel library known as Babel)
+// React Element
 const jsxusing = <div id="container">
-                    <h1> This is First Code Using JSX in React</h1>
+                    <h1> This is First Code Using JSX in React Element</h1>
                     <p>Hello Everyone</p>
                 </div>;
 
 console.log(jsxusing);
-// Both the have the same output as Object because of Babel  
+
+// React Component
+
+const Heading =()=>{
+    return (
+        <div id="container">
+                    <h1> This is First Code Using JSX in React Component</h1>
+                    <p>Hello Everyone</p>
+                </div>
+    );
+}
 
 
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
-root1.render(jsxusing);
+// root1.render(jsxusing);
+
+root1.render(<Heading/>);
