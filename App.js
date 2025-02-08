f57c41263873776e1f5ad116f860b45787804078
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-08 23:43:51 +0530

    Created a Header For Food Delivery Website with components , css and JSX

--- App.js
+++ App.js
@@ -1,69 +1,31 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import logo from './Image/Logo.png';
 
-//  Using JSX
-//  JSX (Transpiled before reaches the JS) => (Transpiled is done by the parcel library known as Babel)
-// React Element
-const jsxusing = <div id="container">
-                    <h1> This is First Code Using JSX in React Element</h1>
-                    <p>Hello Everyone</p>
-                </div>;
-
-console.log(jsxusing);
-
-
-// React Component
-
-const Heading =()=>{
+const Header = () =>{
     return (
-        <div id="container">
-                    <h1> This is First Code Using JSX in React Component</h1>
-                    <p>Hello Everyone</p>
-                </div>
+        <div className="header">
+            <div >
+                <img className="logo" src={logo} alt="Logo"/>
+            </div>
+            <div className="nav-items">
+                <ul className="list">
+                    <li id="n1">Home</li>
+                    <li id="n2">About Us</li>
+                    <li id="n3">Contect Us</li>
+                    <li id="n4">Cart</li>
+                </ul>
+            </div>
+        </div>
     );
 }
-
-
-// Component Compostion
-
-const Header = ()=>{
+const AppLayout = () =>{
     return (
-    <div>
-        <h1>Welcome to my Website</h1>
-    </div>
-    )
-}
-
-const Footer = () => {
-    return(
         <div>
-            <h5>This is the footer of my Website </h5>
+            <Header/>
         </div>
     )
 }
 
-const Body = () => {
-    return (
-        <div>
-        <Header></Header>
-        <Header/>
-        <div>
-            <p>This is the center Body of my Website</p>
-        </div>
-        <Footer/>
-        {Footer()}
-        </div>
-    );
-}
-
-// Three Ways to call Functional Components to another Functional Component
-// 1.) <name of Component></name of Component>
-// 2.) <name of Component/>
-// 3.) {name of Component()}
-
-const root1 = ReactDOM.createRoot(document.getElementById("root"));
-// root1.render(jsxusing);   // Rendering for React Element
-
-// root1.render(<Heading/>);  // Rendering for React Functional Component
-
-root1.render(<Body></Body>);  // Rendering for Component Composition
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
