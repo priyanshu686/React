f11f825c5397935b3aa6258701916ed173e0b8e6
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-08 11:33:16 +0530

    Second way of using react and react dom by installing dependencies form npm server

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
 const heading = React.createElement('h1', {}, "This is First Code in React");
         
 const div1 = React.createElement('div', { id: 'container' }, heading , "Hello Everyone");
 
-const root1 = ReactDOM.createRoot(document.body);
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root1.render(div1);
