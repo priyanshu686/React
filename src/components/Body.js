0c4fa87fb35092b5b690a31e583263344ce6b0d6
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-14 12:46:45 +0530

    put fetch link in constants.js for good pratice and clean and readable code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 // import { data } from "../utils/LiveData.js";
 import ResCard from "./ResCard.js";
 import { useState, useEffect } from "react";
+import { fetchlink } from "../utils/constants.js";
 
 const Body = () => {
   const [list, setlist] = useState([]);
@@ -11,8 +12,7 @@ const Body = () => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const response = await fetch(fetchlink);
       const data = await response.json();
       setlist(data?.data?.cards[1].card?.card?.gridElements?.infoWithStyle?.restaurants);
     } catch (error) {
