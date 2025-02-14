05767ed6c919c944451d546d478ab1224129d900
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-14 15:31:43 +0530

    Add login and Logout Button using useState

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import logo from '../../Image/Logo.png';
 
 const Header = () =>{
+    const [btnName,setbtnName] = useState("Login");
+
     return (
         <div className="header">
             <div >
@@ -12,7 +15,13 @@ const Header = () =>{
                     <li id="n2">About Us</li>
                     <li id="n3">Contect Us</li>
                     <li id="n4">Cart</li>
+                    <button id="login"
+                onClick={()=>{
+                    btnName === "Login" ? setbtnName("Logout"):setbtnName("Login")
+                }}
+                >{btnName}</button>
                 </ul>
+                
             </div>
         </div>
     );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const imglink = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+
 export const fetchlink = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
