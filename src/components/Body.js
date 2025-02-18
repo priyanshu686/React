996c02ca6a0175cbe4e244e19ac8024d6127d87c
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-18 16:56:21 +0530

    Higher Order Component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,68 +1,91 @@
 // import { data } from "../utils/LiveData.js";
-import ResCard from "./ResCard.js";
+import ResCard,{addDiscount} from "./ResCard.js";
 import { useState, useEffect } from "react";
 import { fetchlink } from "../utils/constants.js";
 import Shimmer from "./Shimmer.js";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Body = () => {
   const [list, setlist] = useState([]);
-  const[filterlist,setfilterlist] = useState([]);
-  const[searchText,setsearchText] = useState("");
+  const [filterlist, setfilterlist] = useState([]);
+  const [searchText, setsearchText] = useState("");
   // console.log("After the UseState");
   useEffect(() => {
     fetchData();
     // console.log("useEffect is Rendered");
   }, []);
-
+  
   const fetchData = async () => {
     try {
       const response = await fetch(fetchlink);
       const data = await response.json();
-      setlist(data?.data?.cards[1].card?.card?.gridElements?.infoWithStyle?.restaurants);
-      setfilterlist(data?.data?.cards[1].card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setlist(
+        data?.data?.cards[1].card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setfilterlist(
+        data?.data?.cards[1].card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
-  if(list.length === 0){
-    return <Shimmer/>;
+  if (list.length === 0) {
+    return <Shimmer />;
   }
-
-  return ( 
+  const Discount = addDiscount(ResCard);
+  const res = (restaurant) => {
+    const discountInfo = restaurant.info?.aggregatedDiscountInfoV3;
+    if (discountInfo && Object.keys(discountInfo).length === 0) {
+      return <ResCard resobj={restaurant} />;
+    } else {
+      return <Discount resobj={restaurant} />;
+    }
+  };
+  return (
     <div className="distanceBody">
       {/* {console.log("Body Render")}; */}
       <div className="search-top_res">
-        <input type="text" 
-        className="search-box"
-        value={searchText} 
-        onChange={(e)=>{
-          setsearchText(e.target.value);
-        }}
+        <input
+          type="text"
+          className="search-box"
+          value={searchText}
+          onChange={(e) => {
+            setsearchText(e.target.value);
+          }}
         />
-        <button onClick={()=>{
-          const filterdata = list.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
-          setfilterlist(filterdata);
-        }}>
+        <button
+          onClick={() => {
+            const filterdata = list.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setfilterlist(filterdata);
+          }}
+        >
           Search
         </button>
-      <button className="top-res"
-        onClick={() => {
-          const filterdata = list.filter((res) => res.info.avgRating > 4.5);
-          setfilterlist(filterdata);
-        }}>
-        Top Rated Restaurants
-      </button>
+        <button
+          className="top-res"
+          onClick={() => {
+            const filterdata = list.filter((res) => res.info.avgRating > 4.5);
+            setfilterlist(filterdata);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="res-container">
-  {filterlist.map((restaurent) => (
-    <Link to={`/restaurants/${restaurent.info.id}`} key={restaurent.info.id}>
-      <ResCard resobj={restaurent} />
-    </Link>
-  ))}
-</div>
-
+        {filterlist.map((restaurent) => (
+          <Link
+            to={`/restaurants/${restaurent.info.id}`}
+            key={restaurent.info.id}
+          >
+            {res(restaurent)}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -1,37 +1,56 @@
 import { imglink } from "../utils/constants";
 
+const ResCard = (props) => {
+  const { resobj } = props;
+  const { cloudinaryImageId, name, cuisines, costForTwo, locality, avgRating } =
+    resobj?.info || {};
+  const { lastMileTravel } = resobj?.info?.sla || {};
+  console.log(resobj);
+  // let rating;
+  // if(ratingV2 === '-'){
+  //     rating = "NEW";
+  // }
+  // else{
+  //     rating = ratingV2 + "Stars";
+  // }
+  return (
+    <div className="res-card">
+      <img
+        className="res-img"
+        alt="Restaurant-Image"
+        src={`${imglink}${cloudinaryImageId}`}
+      />
 
-const ResCard = (props) =>{
-    const {resobj} = props;
-    const {cloudinaryImageId,name,cuisines,costForTwo,locality,avgRating} = resobj?.info || {};
-    const {lastMileTravel} = resobj?.info?.sla || {};
-    console.log(resobj);
-    // let rating;
-    // if(ratingV2 === '-'){
-    //     rating = "NEW";
-    // }
-    // else{
-    //     rating = ratingV2 + "Stars";
-    // }
+      <div className="res-name">
+        <h3>{name}</h3>
+        <p className="star-rate">{avgRating} Stars</p>
+      </div>
+      <div className="cuisine">
+        {/* <p>{cuisines.map(cuisineItem => cuisineItem.name).join(', ')}</p> */}
+        <p>{cuisines[0] + "," + cuisines[1]}</p>
+        <p>{costForTwo}</p>
+      </div>
+      <div className="address">
+        <p>{locality}</p>
+        <p>{lastMileTravel} km</p>
+      </div>
+    </div>
+  );
+};
+
+export const addDiscount = (ResCard) => {
+  return (props) => {
+    const{resobj} = props;
     return (
-        <div className="res-card">
-            <img className="res-img" alt="Restaurant-Image" src={`${imglink}${cloudinaryImageId}`} />
-
-            <div className="res-name">
-                <h3>{name}</h3>
-                <p className="star-rate">{avgRating} Stars</p>
-            </div>
-            <div className="cuisine">
-                {/* <p>{cuisines.map(cuisineItem => cuisineItem.name).join(', ')}</p> */}
-                <p>{cuisines[0]+","+cuisines[1]}</p>
-                <p>{costForTwo}</p>
-            </div>
-            <div className="address">
-                <p>{locality}</p>
-                <p>{lastMileTravel} km</p>
-            </div>
-        </div>
-    )
-}
+      <div>
+        <label className="dis">
+          {resobj?.info?.aggregatedDiscountInfoV3?.header} {" "}
+          {resobj?.info?.aggregatedDiscountInfoV3?.subHeader}{" "}
+        </label>
+        <ResCard {...props} />
+      </div>
+    );
+  };
+};
 
 export default ResCard;
