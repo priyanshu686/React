fd33a58f495120eedb21a44cbeafb6a68e19bd4a
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-19 16:36:01 +0530

    Prettier

--- src/components/Restaurant_Menu.js
+++ src/components/Restaurant_Menu.js
@@ -1,26 +1,20 @@
-import { useState } from 'react';
-import Restaurant_itemCard from './Restaurant_ItemCard';
+import { useState } from "react";
+import Restaurant_itemCard from "./Restaurant_ItemCard";
 
-const Restaurant_Menu = ({ data , showitem , setshowindex}) => {
-//   console.log(type[0]?.card?.card?.title);
+const Restaurant_Menu = ({ data, showitem, setshowindex }) => {
+  //   console.log(type[0]?.card?.card?.title);
   const handleClick = () => {
     setshowindex();
   };
 
   return (
     <div>
-      
-        <div  className="res-title">
-          <div className="dish-title" onClick={handleClick}>
-            {data.title}
-          </div>
-          <div>
-            {showitem && 
-              <Restaurant_itemCard card={data.itemCards} />
-            }
-          </div>
+      <div className="res-title">
+        <div className="dish-title" onClick={handleClick}>
+          {data.title}
         </div>
-      
+        <div>{showitem && <Restaurant_itemCard card={data.itemCards} />}</div>
+      </div>
     </div>
   );
 };
